b90109b442ea7d838c2f7866451e520c80369494
Ayo <olubayo.ayobami21@gmail.com>
2023-03-03 09:13:20 +0000

    2nd

--- new.py
+++ new.py
@@ -1,7 +1,13 @@
-print('hello world')
+name = input("Enter programmer's name:")
+print('Hello and welcome genius:', name)
 print("My name is Ayo and i'm learning Python")
 
 
+#Get the input of the european elevator floor
 inp = input('Europe Floor?')
+
+#Add the european floor with the US floor
 usf = int(inp) + 1
+
+#Print the US floor by indenting it by one
 print('US floor', usf)
