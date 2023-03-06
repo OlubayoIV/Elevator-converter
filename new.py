c57c7f47770d4471234dc19a9fd3979d867daafd
Ayo <olubayo.ayobami21@gmail.com>
2023-03-06 22:06:49 +0000

    const

--- new.py
+++ new.py
@@ -2,7 +2,7 @@
 inp = input('Europe Floor?')
 
 #Add the european floor with the US floor
-usf = int(inp) + 1
+us_floor = int(inp) + 1
 
 #Print the US floor by indenting it by one
-print('US floor', usf)
+print('US floor', us_floor)
