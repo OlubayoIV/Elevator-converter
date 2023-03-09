593a7515fe35edf96c6c1c9481cee948448af6b3
Ayo <olubayo.ayobami21@gmail.com>
2023-03-09 03:49:59 +0000

    test

--- new.py
+++ new.py
@@ -6,3 +6,11 @@ us_floor = int(inp) + 1
 
 #Print the US floor by indenting it by one
 print('US floor', us_floor)
+
+found = 'non'
+print('Before', found)
+for value in (9, 31, 12, 3, 15, 41)
+    if value == 3:
+        found = 'oui'
+    print(found, value)
+print('After', found)
